cd4a6fd84f5fbfd4631cdd78e6e26a15aa4c68f4
lisseteventura <lisseteventura@gmail.com>
2019-11-06 06:54:47 -0500

    added two projects to project section

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $(".menu-toggler").on("click", function() {
+    $(this).toggleClass("open");
+    $(".top-nav").toggleClass("open");
+  });
+  // once you click on the links menu closes
+  $(".top-nav .nav-link").on("click", function() {
+    $(".menu-toggler").removeClass("open");
+    $(".top-nav").removeClass("open");
+  });
+});
+$("#up").on("click", function() {
+  $("html, body").animate(
+    {
+      scrollTop: 0
+    },
+    2000
+  );
+});
+
+AOS.init({
+  easing: "ease",
+  duration: 1800
+});
+
+// //when user clicks up button it scrolls to the topFunction
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
